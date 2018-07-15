6c4eccb6656bced7a5159a9de61362348e25a1b2
Derek <benakderek@seattleu.edu>
2018-07-15 00:16:37 +0000

    troubleshooting Javascript formulas

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let isInteger,
 // TODO: Initialize a variable `isInteger` to use as a control value for the
 // `while` loop. Set the initial value to `false`.
 
-isInteger=false;
+isInteger = false;
 
 // TODO: Create a `while` loop so user will be prompted to enter a number until
 // they enter a good number. This loop should be controlled by a conditional
@@ -23,7 +23,7 @@ while(!isInteger) {
    // - Must be greater than zero
    
    // TODO: Inside `while` loop prompt the user for the `maxNumber` value.
-   maxNumber = prompt('Enter a number that is over 0 (must be an integer), 30');
+   maxNumber = prompt('Enter a number over 0 (integer): 20');
    
    // TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
    // is a suitable integer. (It should be an integer greater than zero.)
@@ -33,25 +33,22 @@ while(!isInteger) {
        isInteger = true;
    }
 }
-
-
-
 // TODO: Initialize the `fbResults` variable to an empty Array
 
 fbResults =[];
 
 // TODO: Create a `for` loop that will execute the `maxNumber` of times.
 
-for (let i=1; <=30 i=++){
+for (let i=1; <= maxNumber i++){
     
     // TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
     // require the use of several conditional statements that use the `%` operator.
     // Store the results of this logic in an array called `fbResults`.
-    if (i % 15 =0){
+    if (i % 15 === 0)){
             fbResults.push('FizzBuzz');
-        }else if (i % 3=0) {
+        }else if (i % 3 === 0)) {
             fbResults.push('Fizz');
-        }else if (i % 5=0) {
+        }else if (i % 5 === 0)) {
             fbResults.push('Buzz');
         }else {
             fbResults.push(i);
